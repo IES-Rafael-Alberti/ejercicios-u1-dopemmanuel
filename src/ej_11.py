ad61bbb5e61309396372cf9dcad07c935a7b28f7
dopemmanuel <dopemmanuel@iesrafaelalberti.es>
2024-09-26 23:59:46 +0200

    ヾ(⌐■_■)ノ♪

--- src/ej_11.py
+++ src/ej_11.py
@@ -1,5 +1,14 @@
 """Escribir un programa que lea un entero positivo, n, introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta n. La suma de los n primeros enteros positivos puede ser calculada de la siguiente forma:"""
 
+while True:
+    try:
+        n = int(input("Ingrese el numero de veces: "))
+        if n > 0:
+            suma = n * (n + 1) / 2
+            print(f"La suma de los enteros de 1 a {n} es: {suma}")
+            break
+        else:
+            print("Ese no es un numero positivo")
 
-n = int(input("Ingrese el numero de veces: "))
-
+    except ValueError:
+        print("Solo de caracter numerico.")
---
+++ src/ej_14.py
@@ -0,0 +1,12 @@
+"""Una juguetería tiene mucho éxito en dos de sus productos: payasos y muñecas. Suele hacer venta por correo y la empresa de logística les cobra por peso de cada paquete así que deben calcular el peso de los payasos y muñecas que saldrán en cada paquete a demanda. Cada payaso pesa 112 g y cada muñeca 75 g. Escribir un programa que lea el número de payasos y muñecas vendidos en el último pedido y calcule el peso total del paquete que será enviado."""
+
+funny_clowns = 112
+fancy_dolls = 75
+
+
+cantidad1 = int(input("Cuantos payasos de jugete quieres comprar? "))
+cantidad2 = int(input("Cuantas muñecas quieres comprar? "))
+
+peso = (cantidad1 * funny_clowns) + (cantidad2 * fancy_dolls)
+
+print(f"El peso total de su paquete con la cantidad de muñecas: {cantidad2} y con la cantidad de juegetes de payaso: {cantidad1} su peso seria de: {peso} kg")
