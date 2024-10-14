28e26ac19882985a3c9cc8eb0ae764d31103b21d
dopemmanuel <dopemmanuel@iesrafaelalberti.es>
2024-10-14 19:12:05 +0200

    Updated

--- src/Práctica 2.1 Sentencias condicionales/ej_1_SC.py
+++ src/Práctica 2.1 Sentencias condicionales/ej_1_SC.py
@@ -1,1 +1,12 @@
-"""Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no."""
+"""Hola"""
+def adult_swim():
+
+    edad = int(input("Cual es tu edad? → "))
+
+
+
+
+
+    if __name__ == "__main__":
+        print("Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.\n")
+        adult_swim()
